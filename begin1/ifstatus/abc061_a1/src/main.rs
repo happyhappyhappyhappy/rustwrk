dc37cd4d9f59c9260c0d07420de4f76774e17ef8
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-09-23 19:07:35 +0900

    依存一覧の作成とabc061a完了

--- begin1/ifstatus/abc061_a1/src/main.rs
+++ begin1/ifstatus/abc061_a1/src/main.rs
@@ -1,3 +1,19 @@
+use proconio::input;
 fn main() {
-    println!("Hello, world!");
+    input!{
+        first:i32,
+        second:i32,
+        third:i32,
+    }
+    let ans:bool;
+    ans = if (first <= third) && (third <= second){
+        true
+    }else{
+        false
+    };
+    if ans{
+        println!("Yes")
+    }else{
+        println!("No")
+    };
 }
