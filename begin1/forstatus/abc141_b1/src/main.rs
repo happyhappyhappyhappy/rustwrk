b74382101651d7427767364bd79ce4a1b9d4a5b6
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-09-26 18:13:57 +0900

    Check if the string follows a specific pattern in a for loop

--- begin1/forstatus/abc141_b1/src/main.rs
+++ begin1/forstatus/abc141_b1/src/main.rs
@@ -5,6 +5,19 @@ fn main() {
         strs:Chars,
     }
     let strlen:usize=strs.len();
-    // TODO: 全検索 外れを引けば良いのか
-    println!("{}",strlen);
+     let mut result:bool=true;
+    for j in 0..strlen{
+        let o_or_e:char;
+        o_or_e = if j % 2 == 0 {
+            'L'
+        }else{
+            'R'
+        };
+        result = if strs[j] == o_or_e{
+            false
+        }else{
+            result
+        };
+    }
+    if result {println!("Yes")} else{println!("No")};
 }
---
+++ begin1/forstatus/problem.md
@@ -0,0 +1,5 @@
+# For文
+
+* それ以外は2回目以降
+* [ABC141B](https://atcoder.jp/contests/abc141/tasks/abc141_b)
+
